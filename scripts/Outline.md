315d130c20fdae9ad4b3dedde72b52448b261c16
Julin Maloof <jnmaloof@ucdavis.edu>
2022-12-28 21:03:12 -1000

    Basic split image working

--- scripts/Outline.md
+++ scripts/Outline.md
@@ -3,9 +3,11 @@
     brew install graphicsmagick
     conda create -n textract python=3.10.8
     conda activate textract
-    conda install -n textract boost
+    #conda install -n textract boost #only if we need graphicsmagick
+    conda install -n textract poppler # needed for pdf2image
     pip install boto3 #aws bindings
-    pip install pgmagick #grapicsmagick bindings
+    #pip install pgmagick #grapicsmagick bindings
+    pip install pdf2image
 
 ## Outline of steps
 
--- scripts/splitImages.py
+++ scripts/splitImages.py
@@ -1,6 +1,20 @@
 # python script to split up a PDF into multiple jpegs
 # start off hard coding it, but will add arguments later
 
-from pgmagick import gminfo
+from pdf2image import convert_from_path
+from os import path
 
-pdfpath = "~/Library/CloudStorage/GoogleDrive-jnmaloof@ucdavis.edu/Shared drives/IntBioTeam/Common Gardens/Data Scans/Size_survey_20221128.pdf"
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+
+pdfpath = "/Users/jmaloof/Library/CloudStorage/GoogleDrive-jnmaloof@ucdavis.edu/Shared drives/IntBioTeam/Common Gardens/Data Scans/Size_survey_20221128.pdf"
+
+file = path.basename(pdfpath)
+
+file = path.splitext(file)[0] + "-"
+
+images = convert_from_path(pdfpath, dpi=300, output_folder = "test_folder", fmt="jpg", output_file=file)
